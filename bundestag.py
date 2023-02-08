c6642f0ac43b2dda0c70d37556d2c9f510112263
Maxelone <maxelone@yandex.ru>
2023-02-09 01:48:26 +0400

    ready parser ig

--- bundestag.py
+++ bundestag.py
@@ -1,25 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-# with open('scrape learning/bundestag.de/persons_links.txt') as file:  # save links in new list and strip them to be sure
-#     lines = [line.strip() for line in file.readlines()]
+if __name__ == '__main__':
+    with open('scrape learning/bundestag.de/persons_links.txt') as file:  # save links in new list and strip them to be sure
+        links = [link.strip() for link in file.readlines()]
 
-#     for line in lines:
-#         response = requests.get(line)
-#         soup = BeautifulSoup(response.content, 'lxml')
-#         # go to one of links in persons_links.txt, check how to get needed data
-#         person = soup.select('.bt-biografie-name h3').text  # col-xs-8 col-md-9 bt-biografie-name
-#         person_name_company = person.strip().split(',')  # example: Sanae Abdi, SPD
-#         person_name = person_name_company[0]
-#         person_company = person_name_company[1]
+        data_dict = {}
 
+        # creating counter
+        count = 0
+        for link in links:
+            response = requests.get(link)
+            soup = BeautifulSoup(response.content, 'lxml')
+            # go to one of links in persons_links.txt, check how to get needed data
+            person = soup.select('.bt-biografie-name h3').text  # col-xs-8 col-md-9 bt-biografie-name
+            person_name_company = person.strip().split(',')  # example: Sanae Abdi, SPD
+            person_name = person_name_company[0]
+            person_company = person_name_company[1].strip()
+            # <a title="Homepage" href="https://sanae-abdi.spd.de/" class="bt-link-extern">Homepage</a>
+            social_networks = soup.find_all(class_='bt-link-extern')
 
-response = requests.get('https://www.bundestag.de/en/members/abdi_sanae-861028')
-soup = BeautifulSoup(response.content, 'lxml')
-# go to one of links in persons_links.txt, check how to get needed data
-person = soup.find(class_='bt-biografie-name').find('h3').text  # col-xs-8 col-md-9 bt-biografie-name
-person_name_company = person.strip().split(',')  # example: Sanae Abdi, SPD
-person_name = person_name_company[0]
-person_company = person_name_company[1]
+            social_networks_links = []
+            for item in social_networks:
+                social_networks_links.append(item['href'])
+            
+            data = {
+                'person_name': person_name,
+                'company_name': person_company,
+                'social_networks': social_networks_links
+            }
 
-print(person_name, person_company)
+            count += 1
+            print(f'#{count}: {link} is done.')
+
+            data_dict.append(data)
+
+            # create json file for write all parsed data 
+            with open('data.json', 'w') as f:
+                json.dump(data_dict, f, indent=4)
