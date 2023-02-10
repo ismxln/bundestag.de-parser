1ec9ee3bdeca5fc959dfb1cbb7e65dd2e8e6a4cc
Maxelone <maxelone@yandex.ru>
2023-02-10 15:42:26 +0400

    faster

--- bundestag.py
+++ bundestag.py
@@ -2,31 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 import json
 from time import sleep
-import random
+from random import randrange
+# from fake_useragent import UserAgent
 
-if __name__ == '__main__':
-    with open('persons_links.txt') as file:  # save links in new list and strip them to be sure
-        links = [link.strip() for link in file.readlines()]
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0'
+}
 
-        data_dict = []
+with open('persons_links.txt') as file:  # save links in new list and strip them to be sure
+    links = [link.strip() for link in file.readlines()]
 
-        # creating counter
-        count = 0
-        for link in links:
-            response = requests.get(link)
+    data_dict = []
+
+    count = 0
+
+    for link in links:
+        response = requests.get(url=link, headers=headers)
+
+        print(f'|status_code={response.status_code}|')
+        
+        if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'lxml')
+
             # go to one of links in persons_links.txt, check how to get needed data
-            person = soup.find(class_='bt-biografie-name').find('h3').text  # col-xs-8 col-md-9 bt-biografie-name
-            person_name_company = person.strip().split(',')  # example: Sanae Abdi, SPD
+            person = soup.find('div', class_='bt-biografie-name').find('h3').text.strip()
+            print(person)  # <h3> Sanae Abdi, SPD</h3> (without .text.strip())
+            person_name_company = person.split(',')
             person_name = person_name_company[0]
             person_company = person_name_company[1].strip()
-            # <a title="Homepage" href="https://sanae-abdi.spd.de/" class="bt-link-extern">Homepage</a>
-            social_networks = soup.find_all(class_='bt-link-extern')
 
+            # <a title="Homepage" href="https://sanae-abdi.spd.de/" class="bt-link-extern">Homepage</a>
+            
+            social_networks = soup.find_all(class_='bt-link-extern')
             social_networks_links = []
             for item in social_networks:
                 social_networks_links.append(item['href'])
-            
+
             data = {
                 'person_name': person_name,
                 'company_name': person_company,
@@ -34,11 +45,22 @@ if __name__ == '__main__':
             }
 
             count += 1
-            sleep(random.randrange(2, 4))
-            print(f'#{count}: {link} is done.')
 
+            print(f'#{count}: {link} is done.')
             data_dict.append(data)
 
+            if count % 30 == 0:
+                sleep(randrange(5, 8))
+
             # create json file for write all parsed data 
-            with open('data.json', 'w') as f:
-                json.dump(data_dict, f, indent=4)
+        else:
+            print(response.text)
+            print(f'well.. {response}')
+            break
+
+        
+with open('data.json', 'w') as f:
+    json.dump(data_dict, f, indent=4, ensure_ascii=False)
+        
+        
+        
